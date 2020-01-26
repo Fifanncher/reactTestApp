484bd21f1823b8adba36a4817e958ef64781be28
Fifanncher <fifanncher@gmail.com>
2020-01-26 12:33:12 +0500

    29lesson

---
+++ src/redux/state.js
@@ -0,0 +1,27 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, message: "Hi oldmans!", likesCount: 27 },
+      { id: 2, message: "I am 54 years old", likesCount: 228 }
+    ]
+  },
+  dialogsPage: {
+    dialogs: [
+      { id: 1, name: "Denchik" },
+      { id: 2, name: "Valera" },
+      { id: 3, name: "Glad" },
+      { id: 4, name: "Vlad" },
+      { id: 5, name: "Danila" },
+      { id: 6, name: "German" }
+    ],
+    messages: [
+      { id: 1, message: "DA DA YA" },
+      { id: 2, message: "OPA" },
+      { id: 3, message: "ZIGOTA" },
+      { id: 4, message: "PENELOPA" },
+      { id: 5, message: "DA DA" }
+    ]
+  }
+};
+
+export default state;
